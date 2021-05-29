54859e103df874947d8c0f4d16c2ebf8e9807cee
LittleWhiteYA <f46284628@gmail.com>
2021-05-29 21:46:56 +0800

    feat(chips): remove weekly threshold

--- chip_stats.py
+++ chip_stats.py
@@ -8,16 +8,7 @@ load_dotenv()
 
 MONGO_URL = os.environ.get("MONGO_URL")
 COMPANY_CODE = os.environ.get("COMPANY_CODE")
-WEEKLY_THRESHOLD = os.environ.get("WEEKLY_THRESHOLD")
-TOTAL_THRESHOLD = os.environ.get("TOTAL_THRESHOLD")
-
-if not COMPANY_CODE:
-    COMPANY_CODE = input("input company code: ")
-
-if not COMPANY_CODE:
-    raise ValueError(COMPANY_CODE, "COMPANY_CODE is missing")
-
-company_code = f"company_{COMPANY_CODE}"
+THRESHOLD = os.environ.get("THRESHOLD")
 
 company_default_threshold = [
     {
@@ -27,6 +18,10 @@ company_default_threshold = [
     {
         "company_code": "company_1786",
         "default_threshold": 100,
+    },
+    {
+        "company_code": "company_2065",
+        "default_threshold": 150,
     },
     {
         "company_code": "company_2352",
@@ -62,13 +57,17 @@ company_default_threshold = [
     },
 ]
 
-if WEEKLY_THRESHOLD:
-    weekly_big_trader_threshold = int(WEEKLY_THRESHOLD)
-else:
-    weekly_big_trader_threshold = 0
+if not COMPANY_CODE:
+    COMPANY_CODE = input("input company code: ")
 
-if TOTAL_THRESHOLD:
-    total_big_trader_threshold = int(TOTAL_THRESHOLD)
+if not COMPANY_CODE:
+    raise ValueError(COMPANY_CODE, "COMPANY_CODE is missing")
+
+company_code = f"company_{COMPANY_CODE}"
+
+
+if THRESHOLD:
+    big_trader_threshold = int(THRESHOLD)
 else:
     threshold = next(
         (
@@ -79,71 +78,74 @@ else:
         None,
     )
 
-    total_big_trader_threshold = threshold if threshold else 100
+    big_trader_threshold = threshold if threshold else 100
 
 mongo_client = MongoClient(MONGO_URL)
 db = mongo_client.get_default_database()
 
-weekly_trades = list(db[company_code].find({}))
 
-if not weekly_trades:
-    raise ValueError(
-        weekly_trades, f"company code {company_code} may not be crawled yet"
-    )
+def main():
+    weekly_trades = list(db[company_code].find({}))
 
-weekly_trades_stats = []
-
-for weekly_trade in weekly_trades:
-    count_weekly_traders = map(
-        lambda trade: {
-            **trade,
-            "quantitiesCount": trade["buyQuantities"] - trade["sellQuantities"],
-        },
-        weekly_trade["data"],
-    )
-    big_traders = list(
-        filter(
-            lambda trade: abs(trade["quantitiesCount"]) >= weekly_big_trader_threshold,
-            count_weekly_traders,
+    if not weekly_trades:
+        raise ValueError(
+            weekly_trades, f"company code {company_code} may not be crawled yet"
         )
+
+    weekly_trades_stats = []
+
+    for weekly_trade in weekly_trades:
+        count_weekly_traders = map(
+            lambda trade: {
+                **trade,
+                "quantitiesCount": trade["buyQuantities"] - trade["sellQuantities"],
+            },
+            weekly_trade["data"],
+        )
+        big_traders = list(count_weekly_traders)
+
+        weekly_trades_stats.append(
+            {trader["agentName"]: trader["quantitiesCount"] for trader in big_traders}
+        )
+
+    weekly_trades_df = pd.DataFrame(
+        weekly_trades_stats, index=map(lambda trade: trade["sinceDate"], weekly_trades)
     )
 
-    weekly_trades_stats.append(
-        {trader["agentName"]: trader["quantitiesCount"] for trader in big_traders}
+    # fill NaN to 0 and do cumsum
+    cusum_trades_df = weekly_trades_df.fillna(0).cumsum()
+
+    # filter columns according to last row value
+    big_trader_filter = abs(cusum_trades_df[-1:].squeeze()) > big_trader_threshold
+
+    big_trades_df = cusum_trades_df.loc[:, big_trader_filter]
+
+    # sort columns according to last row value
+    big_trades_df = big_trades_df.sort_values(
+        big_trades_df.last_valid_index(), axis="columns", ascending=False
     )
 
-weekly_trades_df = pd.DataFrame(
-    weekly_trades_stats, index=map(lambda trade: trade["sinceDate"], weekly_trades)
-)
+    #  print(big_trades_df.to_markdown())
 
-# fill NaN to 0 and do cumsum
-cusum_trades_df = weekly_trades_df.fillna(0).cumsum()
+    # from matplotlib import font_manager
+    # font_set = {f.name for f in font_manager.fontManager.ttflist}
+    plt.rcParams["font.sans-serif"] = "Noto Sans CJK JP"
+    plt.rcParams["figure.dpi"] = 200
 
-# filter columns according to last row value
-big_trader_filter = abs(cusum_trades_df[-1:].squeeze()) > total_big_trader_threshold
+    ax = big_trades_df.plot(
+        figsize=(10, 5), fontsize=15, style="o-", grid=True, legend=2
+    )
+    ax.set_title(
+        f"{company_code} (threshold: {big_trader_threshold})",
+        fontsize=30,
+    )
+    ax.set_xlabel("date", fontsize=10)
+    ax.set_ylabel("count", fontsize=10)
+    ax.legend(loc="upper left", prop={"size": 15})
 
-big_trades_df = cusum_trades_df.loc[:, big_trader_filter]
+    #  plt.show()
+    ax.figure.savefig(f"{company_code}.png")
 
 
-# sort columns according to last row value
-big_trades_df = big_trades_df.sort_values(
-    big_trades_df.last_valid_index(), axis="columns", ascending=False
-)
-
-#  print(big_trades_df.to_markdown())
-
-# from matplotlib import font_manager
-# font_set = {f.name for f in font_manager.fontManager.ttflist}
-plt.rcParams["font.sans-serif"] = "Noto Sans CJK JP"
-plt.rcParams["figure.dpi"] = 200
-
-ax = big_trades_df.plot(figsize=(20, 10), fontsize=15, style="o-", grid=True, legend=2)
-ax.set_title(
-    f"{company_code} (threshold: {weekly_big_trader_threshold}, {total_big_trader_threshold})",
-    fontsize=30,
-)
-ax.set_xlabel("date", fontsize=10)
-ax.set_ylabel("count", fontsize=10)
-ax.legend(loc="upper left", prop={"size": 10})
-
-plt.show()
+if __name__ == "__main__":
+    main()
