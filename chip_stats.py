3932219651c6b93a8ebc03da9c57804a221926c2
LittleWhiteYA <f46284628@gmail.com>
2021-06-28 00:24:25 +0800

    refactor(chip): rename all company_code to stock_id

--- chip_stats.py
+++ chip_stats.py
@@ -10,65 +10,65 @@ load_dotenv()
 MONGO_URL = os.environ.get("MONGO_URL")
 THRESHOLD = os.environ.get("THRESHOLD")
 
-company_default_threshold = [
+stock_default_threshold = [
     {
-        "company_code": "company_1301",
+        "stock_id": "1301",
         "default_threshold": 4000,
     },
     {
-        "company_code": "company_1539",
+        "stock_id": "1539",
         "default_threshold": 100,
     },
     {
-        "company_code": "company_1786",
+        "stock_id": "1786",
         "default_threshold": 100,
     },
     {
-        "company_code": "company_1810",
+        "stock_id": "1810",
         "default_threshold": 500,
     },
     {
-        "company_code": "company_2065",
+        "stock_id": "2065",
         "default_threshold": 150,
     },
     {
-        "company_code": "company_2352",
+        "stock_id": "2352",
         "default_threshold": 5000,
     },
     {
-        "company_code": "company_2451",
+        "stock_id": "2451",
         "default_threshold": 1000,
     },
     {
-        "company_code": "company_4930",
+        "stock_id": "4930",
         "default_threshold": 400,
     },
     {
-        "company_code": "company_4994",
+        "stock_id": "4994",
         "default_threshold": 100,
     },
     {
-        "company_code": "company_5425",
+        "stock_id": "5425",
         "default_threshold": 900,
     },
     {
-        "company_code": "company_5490",
+        "stock_id": "5490",
         "default_threshold": 150,
     },
     {
-        "company_code": "company_6505",
+        "stock_id": "6505",
         "default_threshold": 2000,
     },
     {
-        "company_code": "company_8446",
+        "stock_id": "8446",
         "default_threshold": 200,
     },
     {
-        "company_code": "company_9924",
+        "stock_id": "9924",
         "default_threshold": 200,
     },
     {
-        "company_code": "company_9958",
+        "stock_id": "9958",
         "default_threshold": 700,
     },
 ]
@@ -78,28 +78,27 @@ mongo_client = MongoClient(MONGO_URL)
 db = mongo_client.get_default_database()
 
 
-def get_company_code():
-    company_code = input("input company code: ")
+def get_stock_id():
+    stock_id = input("input stock id: ")
 
-    if company_code == "all":
+    if not stock_id:
+        raise ValueError(stock_id, "stock_id is missing")
+
+    if stock_id == "all":
         return "all"
 
-    if not company_code:
-        raise ValueError(company_code, "company_code is missing")
-
-    company_code = f"company_{company_code}"
-    return company_code
+    return stock_id
 
 
-def get_threshold(company_code):
+def get_threshold(stock_id):
     if THRESHOLD:
         big_trader_threshold = int(THRESHOLD)
     else:
         threshold = next(
             (
                 config["default_threshold"]
-                for config in company_default_threshold
-                if config["company_code"] == company_code
+                for config in stock_default_threshold
+                if config["stock_id"] == stock_id
             ),
             None,
         )
@@ -109,14 +108,15 @@ def get_threshold(company_code):
     return big_trader_threshold
 
 
-def main(company_code, big_trader_threshold):
+def main(stock_id, big_trader_threshold):
+    col_name = f"company_{stock_id}"
     weekly_trades = list(
-        db[company_code].find({"sinceDate": {"$gte": datetime(2021, 1, 1)}})
+        db[col_name].find({"sinceDate": {"$gte": datetime(2021, 1, 1)}})
     )
 
     if not weekly_trades:
         raise ValueError(
-            weekly_trades, f"company code {company_code} may not be crawled yet"
+            weekly_trades, f"stock id {stock_id} may not be crawled yet"
         )
 
     weekly_trades_stats = []
@@ -169,7 +169,7 @@ def main(company_code, big_trader_threshold):
     )
 
     ax.set_title(
-        f"{company_code} (threshold: {big_trader_threshold})",
+        f"{stock_id} (threshold: {big_trader_threshold})",
         fontsize=30,
     )
     ax.set_xlabel("date", fontsize=10)
@@ -179,22 +179,24 @@ def main(company_code, big_trader_threshold):
 
     ax.legend(loc="upper left", prop={"size": 15})
 
-    print(f"generate {company_code} png")
+    print(f"generate {stock_id} png")
     #  plt.show()
-    ax.figure.savefig(f"png/{company_code}.png")
+    ax.figure.savefig(f"png/{stock_id}.png")
 
 
 if __name__ == "__main__":
-    company_code = get_company_code()
+    stock_id = get_stock_id()
 
-    if company_code == "all":
-        for company in company_default_threshold:
-            company_code = company["company_code"]
-            big_trader_threshold = get_threshold(company_code)
-            main(company_code, big_trader_threshold)
+    if stock_id == "all":
+        existed_stocks = db["stocks"].find({"shouldSkip": False}, sort=[("stockId", 1)])
+
+        for stock in existed_stocks:
+            stock_id = stock["stockId"]
+            big_trader_threshold = get_threshold(stock_id)
+            main(stock_id, big_trader_threshold)
     else:
         while True:
-            big_trader_threshold = get_threshold(company_code)
-            main(company_code, big_trader_threshold)
+            big_trader_threshold = get_threshold(stock_id)
+            main(stock_id, big_trader_threshold)
 
-            company_code = get_company_code()
+            stock_id = get_stock_id()
