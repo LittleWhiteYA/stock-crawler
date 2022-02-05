0dce4712bb291251f5ff68a3de388f8b21fc0da1
Johnny You <f46284628@gmail.com>
2022-02-06 01:09:03 +0800

    refactor: add input to skip some part

--- src/update_stock_infos.py
+++ src/update_stock_infos.py
@@ -29,15 +29,16 @@ def get_stock_id():
 
 
 def main():
-    stock_id = get_stock_id()
+    stock_id_input = get_stock_id()
 
-    if stock_id == "all":
+    if stock_id_input == "all":
         existed_stocks = db["stocks"].find({"shouldSkip": False}, sort=[("stockId", 1)])
         #  existed_stocks = db["stocks"].find({}, sort=[("stockId", 1)])
         existed_stock_ids = list(map(lambda stock: stock["stockId"], existed_stocks))
     else:
-        existed_stock_ids = [stock_id]
+        existed_stock_ids = [stock_id_input]
 
+    since_date = None
     if SINCE_DATE:
         since_date = datetime.strptime(SINCE_DATE, "%Y-%m-%d")
 
@@ -47,78 +48,90 @@ def main():
         until_date = datetime.now()
 
     date_column = "日期"
+    print(f"env since_date: {since_date}")
+    print(f"env until_date: {until_date}")
 
-    print("update stock chips")
-    chips_collection = "chips"
+    should_continue = input(f"should update stock '{stock_id_input}' chips (y/n) ?")
 
-    for stock_id in existed_stock_ids:
-        latest_data = db[chips_collection].find_one(
-            {"stockId": stock_id}, sort=[(date_column, -1)]
-        )
-        since_date = (
-            latest_data[date_column] + timedelta(days=1) if latest_data else since_date
-        )
-        print(f"stock id: {stock_id}")
-        print(f"since_date: {since_date}")
-        print(f"until_date: {until_date}")
+    if should_continue == "y":
+        print("update stock chips")
+        chips_collection = "chips"
 
-        chips = get_stock_chips(stock_id, since_date, until_date)
-
-        now = datetime.now()
-
-        for chip in chips:
-            chip["createdAt"] = now
-
-            db[chips_collection].update_one(
-                {
-                    "stockId": chip["stockId"],
-                    date_column: chip[date_column],
-                },
-                {
-                    "$setOnInsert": chip,
-                },
-                upsert=True,
+        for stock_id in existed_stock_ids:
+            latest_data = db[chips_collection].find_one(
+                {"stockId": stock_id}, sort=[(date_column, -1)]
             )
 
-    prices_collection = "dailyPrices"
-    print(f"update stock daily prices in collection {prices_collection}")
+            if since_date:
+                tmp_since_date = since_date
+            else:
+                tmp_since_date = latest_data[date_column] + timedelta(days=1)
 
-    for stock_id in existed_stock_ids:
-        if int(stock_id) <= 1100:
-            continue
+            print(f"stock id: {stock_id}")
+            print(f"since_date: {tmp_since_date}")
+            print(f"until_date: {until_date}")
 
-        latest_data = db[prices_collection].find_one(
-            {"stockId": stock_id}, sort=[(date_column, -1)]
-        )
+            chips = get_stock_chips(stock_id, tmp_since_date, until_date)
 
-        if since_date:
-            last_until_date = since_date
-        else:
-            latest_data[date_column]
+            now = datetime.now()
 
-        print(f"stock id: {stock_id}")
-        print(f"since_date: {last_until_date}")
+            for chip in chips:
+                chip["createdAt"] = now
 
-        format_daily_prices = get_stock_prices(stock_id, last_until_date, until_date)
+                db[chips_collection].update_one(
+                    {
+                        "stockId": chip["stockId"],
+                        date_column: chip[date_column],
+                    },
+                    {
+                        "$setOnInsert": chip,
+                    },
+                    upsert=True,
+                )
 
-        now = datetime.now()
+    should_continue = input(f"should update stock '{stock_id_input}' prices (y/n) ?")
 
-        for price in format_daily_prices:
-            price["createdAt"] = now
+    if should_continue == "y":
+        prices_collection = "dailyPrices"
+        print(f"update stock daily prices in collection {prices_collection}")
 
-            db[prices_collection].update_one(
-                {
-                    "stockId": price["stockId"],
-                    date_column: price[date_column],
-                },
-                {
-                    "$setOnInsert": price,
-                },
-                upsert=True,
+        for stock_id in existed_stock_ids:
+            if int(stock_id) <= 1100:
+                continue
+
+            latest_data = db[prices_collection].find_one(
+                {"stockId": stock_id}, sort=[(date_column, -1)]
             )
 
-        #  if format_daily_prices:
-        #      db[prices_collection].insert_many(format_daily_prices)
+            if since_date:
+                tmp_since_date = since_date
+            else:
+                tmp_since_date = latest_data[date_column]
+
+            print(f"stock id: {stock_id}")
+            print(f"since_date: {tmp_since_date}")
+            print(f"until_date: {until_date}")
+
+            format_daily_prices = get_stock_prices(stock_id, tmp_since_date, until_date)
+
+            now = datetime.now()
+
+            for price in format_daily_prices:
+                price["createdAt"] = now
+
+                db[prices_collection].update_one(
+                    {
+                        "stockId": price["stockId"],
+                        date_column: price[date_column],
+                    },
+                    {
+                        "$setOnInsert": price,
+                    },
+                    upsert=True,
+                )
+
+            #  if format_daily_prices:
+            #      db[prices_collection].insert_many(format_daily_prices)
 
     mongo_client.close()
 
